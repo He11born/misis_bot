fefff3470fd4f950d3980c429780a765768bb36c
He11born <egoosergacev@gmail.com>
2025-11-30 00:51:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Выводим сообщение о начале работы
-echo "Starting the Telegram Webhook bot..."
-# Запускаем основной файл бота на Python
-python app.py
+
+# Запуск Uvicorn-сервера, который будет слушать на порте 10000 
+# (это стандартный порт на Render). 
+# Он запускает FastAPI-приложение (fastapi_app), 
+# которое находится в файле app.py.
+
+echo "Starting Uvicorn server on port 10000..."
+uvicorn app:fastapi_app --host 0.0.0.0 --port 10000
